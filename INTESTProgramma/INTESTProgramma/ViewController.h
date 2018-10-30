24a597f70e1cdc1c0bf5b7ca3b2919e1d42d6427
intest-cwp <intest-cwp@intest-cwpdemacbook-pro.local>
2018-10-30 16:02:29 +0800

    初始化项目

---
+++ INTESTProgramma/INTESTProgramma/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  INTESTProgramma
+//
+//  Created by intest-cwp on 2018/10/30.
+//  Copyright © 2018年 intest-cwp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ INTESTProgramma/INTESTProgramma/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  INTESTProgramma
+//
+//  Created by intest-cwp on 2018/10/30.
+//  Copyright © 2018年 intest-cwp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
